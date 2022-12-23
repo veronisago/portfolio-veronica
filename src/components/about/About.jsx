9eb4e9b83825ea17c008f48bd660ec822abcb493
Veronica <veronisago1999@gmail.com>
2022-12-22 21:54:35 -0500

    structure

---
+++ src/components/about/About.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import './about.css'
+import ME from '../../assets/me-remove.png'
+import { FaAward } from 'react-icons/fa'
+import { FiUsers, FiFolder } from 'react-icons/fi'
+
+
+const About = () => {
+  return (
+    <section id='about'>
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+      <div className="container about__container">
+        <div className='about__me'>
+          <div className="about__me-image">
+            <img src={ME} alt="About Image" />
+          </div>
+        </div>
+        <div className="about__content">
+          <div className='about__cards'>
+            <article className='about__card'>
+              <FaAward className='about__icon' />
+              <h5>Experience</h5>
+              <small>3+ Years Working</small>
+            </article>
+
+            <article className='about__card'>
+              <FiUsers className='about__icon' />
+              <h5>Clients</h5>
+              <small>200+ Woldwide</small>
+            </article>
+
+            <article className='about__card'>
+              <FiFolder className='about__icon' />
+              <h5>Projects</h5>
+              <small>80+ Completed</small>
+            </article>
+          </div>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rerum dolore 
+            quisquam veniam doloremque tenetur aliquam, quis deleniti similique 
+            alias earum! Repellendus aliquid, reiciendis veritatis nobis modi 
+            delectus eos esse architecto.
+          </p>
+
+          <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default About
---
+++ src/components/nav/Nav.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './nav.css'
+import {BiHome, BiUser, BiBook, BiMessageDetail} from 'react-icons/bi'
+import {RiServiceLine} from 'react-icons/ri'
+import { useState } from 'react'
+
+
+
+const Nav = () => {
+
+const [active, setActive] = useState('#')
+
+  return (
+  <nav>
+    <a href="#" onClick={()=> setActive('#')} className={active === '#' ? 'active' : ''}><BiHome/></a>
+    <a href="#about" onClick={()=> setActive('#about')} className={active === '#about' ? 'active' : ''}><BiUser/></a>
+    <a href="#experience" onClick={()=> setActive('#experience')} className={active === '#experience' ? 'active' : ''}><BiBook/></a>
+    <a href="#services"  onClick={()=> setActive('#services')} className={active === '#services' ? 'active' : ''}><RiServiceLine/></a>
+    <a href="#contact" onClick={()=> setActive('#contact')} className={active === '#contact' ? 'active' : ''}><BiMessageDetail/></a>
+  </nav>
+  )
+}
+
+export default Nav
