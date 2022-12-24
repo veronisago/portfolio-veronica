e020a7f355b36147d865f47448016ceb15fd2eab
Veronica <veronisago1999@gmail.com>
2022-12-24 17:12:49 -0500

    changes

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -88,6 +88,13 @@ const Experience = () => {
                 <small className='text-light'>Experienced</small>
               </div>
             </article>
+            <article className='experience__details'>
+              <BsFillPatchCheckFill className='experience__details-icon'/>
+              <div>
+                <h4>Express JS</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
           </div>
         </div>
       </div>
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -3,28 +3,41 @@ import './portfolio.css'
 import IMG1 from '../../assets/vero-project.png'
 import IMG2 from '../../assets/vero-project2.png'
 import IMG3 from '../../assets/vero-project3.png'
+import IMG4 from '../../assets/portfolio1.jpg'
+import IMG5 from '../../assets/portfolio2.jpg'
+import IMG6 from '../../assets/portfolio3.jpg'
+import IMG7 from '../../assets/portfolio4.jpg'
+import IMG8 from '../../assets/portfolio5.png'
+import IMG9 from '../../assets/portfolio6.jpg'
 
+// import Swiper core and required modules
+import { Pagination } from 'swiper';
 
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
 
 
 const data = [
   {
     id: 1,
-    image: IMG1,
+    image: [IMG1, IMG4, IMG7],
     title: 'Movies App',
     github: 'https://github.com/veronisago/Movies-project',
     demo: 'https://github.com/veronisago'
   },
   {
     id: 2,
-    image: IMG2,
+    image: [IMG2, IMG5, IMG8],
     title: 'Dogs App',
     github: 'https://github.com/veronisago/PI-Dogs-main',
     demo: 'https://github.com/veronisago'
   },
   {
     id: 3,
-    image: IMG3,
+    image: [IMG3, IMG6, IMG9],
     title: 'Ecotourism App, Ecommerce Project',
     github: 'https://github.com/veronisago/PF-IncaAdventure',
     demo: 'https://github.com/veronisago'
@@ -44,7 +57,7 @@ const Portfolio = () => {
             return (
               <article key={id} className='portfolio__item'>
                 <div className="portfolio__item-image">
-                  <img src={image} alt={title} />
+                  <img src={image[0]} alt={title} />
                 </div>
                 <h3>{title}</h3>
                 <div className='portfolio__item-cta'>
