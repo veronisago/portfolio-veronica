cfd4acda26be61d189b7290ea2143028df8ef2b4
Veronica <veronisago1999@gmail.com>
2022-12-24 08:57:33 -0500

    new features

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,11 +1,61 @@
 import React from 'react'
 import './portfolio.css'
+import IMG1 from '../../assets/vero-project.png'
+import IMG2 from '../../assets/vero-project2.png'
+import IMG3 from '../../assets/vero-project3.png'
+
+
+
+
+const data = [
+  {
+    id: 1,
+    image: IMG1,
+    title: 'Movies App',
+    github: 'https://github.com/veronisago/Movies-project',
+    demo: 'https://github.com/veronisago'
+  },
+  {
+    id: 2,
+    image: IMG2,
+    title: 'Dogs App',
+    github: 'https://github.com/veronisago/PI-Dogs-main',
+    demo: 'https://github.com/veronisago'
+  },
+  {
+    id: 3,
+    image: IMG3,
+    title: 'Ecotourism App, Ecommerce Project',
+    github: 'https://github.com/veronisago/PF-IncaAdventure',
+    demo: 'https://github.com/veronisago'
+  }
+]
+
 
 const Portfolio = () => {
   return (
     <section id='portfolio'>
       <h5>My Recent Work</h5>
       <h2>Portfolio</h2>
+
+      <div className="container portfolio__container">
+        {
+          data.map(({ id, image, title, github, demo }) => {
+            return (
+              <article key={id} className='portfolio__item'>
+                <div className="portfolio__item-image">
+                  <img src={image} alt={title} />
+                </div>
+                <h3>{title}</h3>
+                <div className='portfolio__item-cta'>
+                  <a href={github} className='btn' target='_blank' rel="noreferrer noopener">Github</a>
+                  <a href={demo} target='_blank' className='btn btn-primary' rel="noreferrer noopener">Live Demo</a>
+                </div>
+              </article>
+            )
+          })
+        }
+      </div>
     </section>
   )
 }
